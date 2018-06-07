e228553dc3f583d54b57300c8a30b3a18d3272c8
Francisco <fvcneto.master98@gmail.com>
2018-06-07 17:20:50 -0300

    First Commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'author': 'One',
+            'body': 'The one',
+            'title': 'The title one',
+            'create_date': '04/06/2018'  
+        },
+        {
+            'id': 2,
+            'author': 'Two',
+            'body': 'The two',
+            'title': 'The title two',
+            'create_date': '04/06/2018'  
+        },
+        {
+            'id': 3,
+            'author': 'Three',
+            'body': 'The three',
+            'title': 'The title three',
+            'create_date': '04/06/2018'  
+        }
+    ]
+
+    return articles
